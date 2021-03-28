b2d522e194373f8b9243bbe1897e0aed38fbe7a9
nikitashvetsov21 <62443471+nikitashvetsov21@users.noreply.github.com>
2021-03-28 12:27:58 -0700

    add

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,22 @@ $(document).ready(function(){
   $('.banner-slider').slick({
     arrows:true,
     dots:true,
-    slidesToShow: 3
+    slidesToShow: 3,
+    autoplay: true,
+    infinite: true,
+    speed: 1000,
+    adaptiveHeight: false,
+    pauseOnFocus: true,
+    pauseOnHover: true,
+    pauseOnDotsHover: true,
+    easing: 'ease',
+    draggale: true,
+    swipe: true,
+    touchTreshold: 20,
+    touchMove: false,
+    prevArrow: document.querySelector('#prevArrow'),
+    nextArrow: document.querySelector('#nextArrow')
+    
   });
 
 });
