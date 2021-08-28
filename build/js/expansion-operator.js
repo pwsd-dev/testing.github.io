cad1cce32f480da31ce81689057110206cc204f2
Nikita Shvetsov <nshvtsv@gmail.com>
2021-08-28 07:03:58 -0700

    add

---
+++ build/js/expansion-operator.js
@@ -0,0 +1,101 @@
+// (function () {
+
+//     let hi = "Hi";
+
+//     console.log(hi);
+// }());
+
+// let value = "Сюрприз!";
+
+// function f() {
+//     let value = "ближайшее значение";
+
+//     function g() {
+//         debugger; // в консоли: напишите alert(value); Сюрприз!
+//     }
+
+//     return g;
+// }
+
+// let g = f();
+// g();
+
+// function makeCounter() {
+
+//     let count = 0;
+
+//     return function () {
+//         return count++;
+//     };
+// }
+
+// let counter = makeCounter();
+// let counter2 = makeCounter();
+
+// console.log(counter());
+// console.log(counter());
+// console.log(counter());
+
+// console.log(counter2());
+
+
+// function Counter() {
+
+//     let count = 0;
+
+//     this.up = function () {
+//         return ++count;
+//     };
+
+//     this.down = function () {
+//         return --count;
+//     };
+// }
+
+// let counter = new Counter();
+
+// console.log(counter.up());
+// console.log(counter.up());
+// console.log(counter.up());
+// console.log(counter.down());
+// console.log(counter.up());
+// console.log(counter.down());
+// console.log(counter.down());
+// console.log(counter.down());
+// console.log(counter.up());
+// console.log(counter.down());
+// console.log(counter.down());
+// console.log(counter.down());
+// console.log(counter.up());
+// console.log(counter.up());
+// console.log(counter.up());
+
+
+
+
+// function sum(n) {
+
+//     return function (z) {
+//         return n + z;
+//     }
+// };
+
+// console.log(sum(2)(4));
+
+
+//Ниже написана функция, которая взаимодействует
+//с методом filter и возвращает число в промежутке от заданных
+// в первой функции (a, b)
+
+
+function inBetween(a, b) {
+
+    return function (n) {
+        return n >= a && n <= b;
+    };
+
+}
+
+let arr = [1, 2, 3, 4, 5, 6, 7];
+
+console.log(arr.filter(inBetween(2, 4)));
--- src/js/modal.js
+++ src/js/modal.js
@@ -1,63 +1,63 @@
-const modalLinks = document.querySelectorAll('.modalLinks');
-const body = document.querySelector('body');
-const lockPadding = document.querySelectorAll(".lock-padding");
+// const modalLinks = document.querySelectorAll('.modalLinks');
+// const body = document.querySelector('body');
+// const lockPadding = document.querySelectorAll(".lock-padding");
 
-let unlock = true;
+// let unlock = true;
 
-const timeout = 500;
+// const timeout = 500;
 
-if (modalLinks.length > 0) {
+// if (modalLinks.length > 0) {
 
-    for (let index = 0; index < modalLinks.length; index++) {
+//     for (let index = 0; index < modalLinks.length; index++) {
 
-        const modalLink = modalLinks[index];
-        modalLink.addEventListener("click", function (e) {
-            const modalName = modalLink.getAttribute('href').replace('#', '');
-            const currentModal = document.getElementById(modalName);
-            modalOpen(currentModal);
-            e.preventDefault();
-        });
-    }
-}
+//         const modalLink = modalLinks[index];
+//         modalLink.addEventListener("click", function (e) {
+//             const modalName = modalLink.getAttribute('href').replace('#', '');
+//             const currentModal = document.getElementById(modalName);
+//             modalOpen(currentModal);
+//             e.preventDefault();
+//         });
+//     }
+// }
 
-const closeModalIcon = document.querySelectorAll('.close-modal');
+// const closeModalIcon = document.querySelectorAll('.close-modal');
 
-if (closeModalIcon.length > 0) {
+// if (closeModalIcon.length > 0) {
 
-    for (let index = 0; index < closeModalIcon.length; index++) {
+//     for (let index = 0; index < closeModalIcon.length; index++) {
 
-        const el = closeModalIcon[index];
-        el.addEventListener('click', function (e) {
-            modalClose(el.closest('.modal'));
-            e.preventDefault();
-        });
-    }
+//         const el = closeModalIcon[index];
+//         el.addEventListener('click', function (e) {
+//             modalClose(el.closest('.modal'));
+//             e.preventDefault();
+//         });
+//     }
 
-}
+// }
 
-function modalOpen(currentModal) {
-    if (currentModal && unlock) {
-        const modalActive = document.querySelector('.modal.active');
+// function modalOpen(currentModal) {
+//     if (currentModal && unlock) {
+//         const modalActive = document.querySelector('.modal.active');
 
-        if (modalActive) {
-            modalClose(modalActive, false);
-        } else {
-            // bodyLock();
-        }
-        currentModal.classList.add('active');
-        currentModal.addEventListener('click', function (e) {
-            if (!e.target.closest('.modal-content')) {
-                modalClose(e.target.closest('.modal'))
-            }
-        });
-    }
-}
+//         if (modalActive) {
+//             modalClose(modalActive, false);
+//         } else {
+//             // bodyLock();
+//         }
+//         currentModal.classList.add('active');
+//         currentModal.addEventListener('click', function (e) {
+//             if (!e.target.closest('.modal-content')) {
+//                 modalClose(e.target.closest('.modal'))
+//             }
+//         });
+//     }
+// }
 
-function modalClose(modalActive) {
-    if (unlock) {
-        modalActive.classList.remove('active');
-    }
-}
+// function modalClose(modalActive) {
+//     if (unlock) {
+//         modalActive.classList.remove('active');
+//     }
+// }
 
 
 
